71fc5485344e39b0e6ddebf0188630adb40c0c7c
phongtrna0715 <phongtran0715@gmail.com>
2022-03-18 23:38:16 +0700

    Release spit script v1.4

--- bashshell/SplitFolder.sh
+++ bashshell/SplitFolder.sh
@@ -2,14 +2,14 @@
 ###################################################################
 #Script Name    : SpiltFolder
 #Description    : Split parent folers based on folder zise and zip child folders
-#Version        : 1.3
+#Version        : 1.4
 #Notes          : None
 #Author         : phongtran0715@gmail.com
 ###################################################################
 
-_VERSION="SplitFolder - 1.3"
+_VERSION="SplitFolder - 1.4"
 
-Root directory needed to run zip command
+#Root directory needed to run zip command
 ROOT_PATH=(
 	"/mnt/ajplus/Pipeline/_ARCHIVE_INDVCMS/ajplus"
 )
@@ -17,30 +17,36 @@ ROOT_PATH=(
 # Directory hold unsuccesful zip folder
 FAIL_PATH="/mnt/ajplus/Admin/CMS/zipX/Failed/"
 
-# Directory hold all input folders
-ARCHIVE_PATH="/mnt/ajplus/_OUT_Box/Zip_7day_Archive/"
-
 # Log folder store application running log, report log
 LOG_PATH="/mnt/ajplus/Admin/"
 
-# deleted folder size threshold value (equivalent to bytes)
-# input folder with size smaller than the threshold will be deleted
-FOLDER_SIZE_THRESHOLD=$((50 * 1024 * 1024)) #50Mb
+#back up folder
+BACKUP_DIR="/mnt/ajplus/_OUT_Box/Zip_7day_Archive/"
 
-# folder stores output zip file
-OUTPUT_PATH="/mnt/ajplus/_OUT_Box/Upload_To_DMV/"
+# zip work folder 1 under threshold
+DMV_UNDER="/mnt/ajplus/Admin/CMS/ZIP_For_DMV_1/"
+
+# zip work folder 2 over threshold
+DMV_OVER="/mnt/ajplus/Admin/CMS/ZIP_For_DMV_2/"
+
+#upload folder 1 (zip under 750gb)
+UPLOAD_DMV_UNDER="/mnt/ajplus/Admin/CMS/Upload_To_DMV_1/"
+
+#upload folder 2 (zip over 750gb)
+UPLOAD_DMV_OVER="/mnt/ajplus/Admin/CMS/Upload_To_DMV_2/"
 
 #This folder stores all folders that have size small than threshold size
 DELETED_DIR="/mnt/restore/__DEL/"
+
+# deleted folder size threshold value (equivalent to bytes)
+# input folder with size smaller than the threshold will be deleted
+# FOLDER_SIZE_THRESHOLD=$((50 * 1024 * 1024)) #50Mb
+FOLDER_SIZE_THRESHOLD=$((750 * 1024 * 1024 * 1024 * 1024)) #750GiB
 
 TOTAL_FOLDER=0
 TOTAL_PROCESSED_FOLDER=0
 TOTAL_FAILED_FOLDER=0
 TOTAL_IGNORED_FOLDER=0
-
-# A large zip file will be splited to multiple part by each DIVIDE_BASE_SIZE
-# Example : 100m (100 megabyte) or 1g (1 gigabyte)
-DIVIDE_BASE_SIZE=1g #1G
 
 function DEBUG() {
   [ "$_DEBUG" == "dbg" ] && $@ || :
@@ -68,18 +74,28 @@ main(){
     validate=1
   fi
 
-  if [ ! -d "$ARCHIVE_PATH" ]; then
-    printf "Warning! Directory doesn't existed [ARCHIVE_PATH][$ARCHIVE_PATH]\n"
+  if [ ! -d "$DMV_UNDER" ]; then
+    printf "Warning! Directory doesn't existed [DMV_UNDER][$DMV_UNDER]\n"
     validate=1
   fi
 
-  if [ ! -d "$LOG_PATH" ]; then
-    printf "Warning! Directory doesn't existed [LOG_PATH][$LOG_PATH]\n"
+  if [ ! -d "$DMV_OVER" ]; then
+    printf "Warning! Directory doesn't existed [DMV_OVER][$DMV_OVER]\n"
     validate=1
   fi
 
-  if [ ! -d "$OUTPUT_PATH" ]; then
-    printf "Warning! Directory doesn't existed [OUTPUT_PATH][$OUTPUT_PATH]\n"
+  if [ ! -d "$BACKUP_DIR" ]; then
+    printf "Warning! Directory doesn't existed [BACKUP_DIR][$BACKUP_DIR]\n"
+    validate=1
+  fi
+
+  if [ ! -d "$UPLOAD_DMV_UNDER" ]; then
+    printf "Warning! Directory doesn't existed [UPLOAD_DMV_UNDER][$UPLOAD_DMV_UNDER]\n"
+    validate=1
+  fi
+
+  if [ ! -d "$UPLOAD_DMV_OVER" ]; then
+    printf "Warning! Directory doesn't existed [UPLOAD_DMV_OVER][$UPLOAD_DMV_OVER]\n"
     validate=1
   fi
 
@@ -96,66 +112,56 @@ main(){
 		return
 	fi
 
+	# Move file to fase folder
+	find "$ROOT_PATH" -maxdepth 1 -type f -print0 | xargs -0 mv -ft "$FAIL_PATH"
+
 	sub_dirs=$(find "$ROOT_PATH" -maxdepth 1 -type d | tail -n +2)
 	while IFS= read -r dir; do
 		TOTAL_FOLDER=$(($TOTAL_FOLDER + 1))
 		echo
 		echo "*** Processing sub folder ($(du -sh $dir | awk '{printf $1}')): $dir"
 		folder_size=$(du -sb $dir | awk '{printf $1}')
-		if [ $folder_size -lt $FOLDER_SIZE_THRESHOLD ]; then
-			echo "Folder size is smaller then threshold. Ignore!"
-			TOTAL_IGNORED_FOLDER=$(($TOTAL_IGNORED_FOLDER + 1))
-			echo "Moving the directory to deleted path"
-			cp -rf "$dir" "$DELETED_DIR"
-			rm -rf "$dir"
+		folder_name=$(echo $(basename "$dir"))
+		lock_file="/tmp/"$folder_name
+
+		if [ -f "$lock_file" ]; then
+			echo "This folder is processing by an another process. Skip!"
 			continue
-		fi
-		# remove space from dir name
-		folder_name=$(echo $(basename $dir) | tr -d ' ')
-		# create output foler
-		mkdir -p "$OUTPUT_PATH"$folder_name 
-
-		output_folder="$OUTPUT_PATH$folder_name/"
-		zip_file="$dir/$folder_name".zip
-		cd "$dir"
-		zip -r $zip_file ./ >/dev/null 2>&1
-		if [ $? -eq 0 ]; then
-			TOTAL_PROCESSED_FOLDER=$(($TOTAL_PROCESSED_FOLDER + 1))
-			echo "Zip folder successfully"
-			# split zip file to multiple parts
-			zip $zip_file --out $output_folder$folder_name".zip" -s $DIVIDE_BASE_SIZE >/dev/null 2>&1
-			num_child_zip=$(find $output_folder -maxdepth 1 -type f -iname "*.z*" | wc -l)
-			output_file=$output_folder"README.txt"
-
-			echo "The master Adobe folder has been divided into $num_child_zip folders due to zip size" > $output_file
-			echo "" >> $output_file
-			output_files=$(find $output_folder -maxdepth 1 -type f -iname "*.z*")
-			while IFS= read -r file; do
-				echo $file >> $output_file
-			done < <(printf '%s\n' "$output_files")
-			echo "" >> $output_file
-			echo "Please download all zip files and merge them by below command:" >> $output_file
-			echo "zip -FF $folder_name.zip --out $folder_name""_final.zip" >> $output_file
-			# delete original zip
-			rm -rf $zip_file
-			# move original folder to archive folder
-			echo "Moving the directory to archive path"
-			cp -rf "$dir" "$ARCHIVE_PATH" 
-			rm -rf "$dir"
 		else
-			TOTAL_FAILED_FOLDER=$(($TOTAL_FAILED_FOLDER + 1))
-			echo "Error! There is an unexpected error when zipping : "$dir
-			echo "Moving the directory to failed path"
-			cp -rf "$dir" "$FAIL_PATH"
-			rm -f "$dir"
-
+			touch "$lock_file"
 		fi
+
+		if [ $folder_size -lt $FOLDER_SIZE_THRESHOLD ]; then
+			TOTAL_UNDER_FOLDER=$(($TOTAL_UNDER_FOLDER + 1))
+			echo "Copying folder to : $DMV_UNDER"
+			cp -rf "$dir" "$DMV_UNDER"
+
+			zip_file="$DMV_UNDER/$folder_name".zip
+			echo "Start zipping:$zip_file"
+			zip -r $zip_file "$DMV_UNDER$folder_name" >/dev/null 2>&1
+			mv -f "$zip_file" "$UPLOAD_DMV_UNDER"
+		else
+			TOTAL_OVER_FOLDER=$(($TOTAL_OVER_FOLDER + 1))
+			echo "Copying folder to : $DMV_OVER"
+			cp -rf "$dir" "$DMV_OVER"
+
+			zip_file="$DMV_OVER/$folder_name".zip
+			echo "Start zipping:$zip_file"
+			zip -r "$zip_file" "$DMV_OVER$folder_name" >/dev/null 2>&1
+			echo "Moving zip file to: $UPLOAD_DMV_OVER"
+			mv -f "$zip_file" "$UPLOAD_DMV_OVER"
+		fi
+		echo "Moving original folder to backup folder"
+		cp -rf "$dir" "$BACKUP_DIR"
+		# rm -rf "$dir"
+
+		rm -rf "$lock_file"
+		echo "Finish procesing"
 	done < <(printf '%s\n' "$sub_dirs")
 	echo "===================="
 	printf "%10s %-20s : $TOTAL_FOLDER\n" "-" "Total folders"
-	printf "%10s %-20s : $TOTAL_PROCESSED_FOLDER\n" "-" "Processed folders"
-	printf "%10s %-20s : $TOTAL_FAILED_FOLDER\n" "-" "Failed folders"
-	printf "%10s %-20s : $TOTAL_IGNORED_FOLDER\n" "-" "Ignored folders"
+	printf "%10s %-20s : $TOTAL_UNDER_FOLDER\n" "-" "Under threshold folders"
+	printf "%10s %-20s : $TOTAL_OVER_FOLDER\n" "-" "Over threshold folders"
 	echo
 	printf "%10s %-20s : $log_file \n" "-" "Log file"
 	echo "===================="
@@ -167,4 +173,3 @@ log_file="$LOG_PATH/split_folder_log_"$(date +%d%m%y_%H%M)".txt"
 main $1 | while IFS= read -r line; do printf '[%s] %s\n' "$(date '+%Y-%m-%d %H:%M:%S')" "$line"; done | tee "$log_file"
 
 echo "Bye"
-
