6cc2110f208c363ce889a88c6f57c70e51e16030
Tran Ngoc Phong <jack@jack-2.local>
2022-04-08 21:48:43 +0700

    Release SplitFolder version 1.6

--- bashshell/SplitFolder.sh
+++ bashshell/SplitFolder.sh
@@ -2,12 +2,12 @@
 ###################################################################
 #Script Name    : SpiltFolder
 #Description    : Split parent folers based on folder zise and zip child folders
-#Version        : 1.5
+#Version        : 1.6
 #Notes          : None
 #Author         : phongtran0715@gmail.com
 ###################################################################
 
-_VERSION="SplitFolder - 1.5"
+_VERSION="SplitFolder - 1.6"
 
 #Root directory needed to run zip command
 ROOT_PATH=(
@@ -22,12 +22,6 @@ LOG_PATH="/mnt/ajplus/Admin/"
 
 #back up folder
 BACKUP_DIR="/mnt/ajplus/_OUT_Box/Zip_7day_Archive/"
-
-# zip work folder 1 under threshold
-DMV_UNDER="/mnt/ajplus/Admin/CMS/ZIP_For_DMV_1/"
-
-# zip work folder 2 over threshold
-DMV_OVER="/mnt/ajplus/Admin/CMS/ZIP_For_DMV_2/"
 
 #upload folder 1 (zip under 750gb)
 UPLOAD_DMV_UNDER="/mnt/ajplus/Admin/CMS/Upload_To_DMV_1/"
@@ -60,6 +54,15 @@ helpFunction() {
   exit 1
 }
 
+# get file size
+get_file_size() {
+	echo $(stat -c%s "$1")
+}
+
+convert_size() {
+	printf %s\\n $1 | LC_NUMERIC=en_US numfmt --to=iec
+}
+
 ################################################################################
 # Main program                                                                 #
 ################################################################################
@@ -71,16 +74,6 @@ main(){
   validate=0
   if [ ! -d "$FAIL_PATH" ]; then
     printf "Warning! Directory doesn't existed [FAIL_PATH][$FAIL_PATH]\n"
-    validate=1
-  fi
-
-  if [ ! -d "$DMV_UNDER" ]; then
-    printf "Warning! Directory doesn't existed [DMV_UNDER][$DMV_UNDER]\n"
-    validate=1
-  fi
-
-  if [ ! -d "$DMV_OVER" ]; then
-    printf "Warning! Directory doesn't existed [DMV_OVER][$DMV_OVER]\n"
     validate=1
   fi
 
@@ -138,16 +131,19 @@ main(){
 			TOTAL_UNDER_FOLDER=$(($TOTAL_UNDER_FOLDER + 1))
 
 			zip_file="$dir_name/$folder_name".zip
-			echo "Start zipping:$zip_file"
+			echo "Start zipping:$folder_name.zip"
 			zip -r $zip_file "$dir" >/dev/null 2>&1
+			size=$(get_file_size "$zip_file")
+			echo "Moving zip file ($(convert_size "$size")) to: $(echo $(basename "$UPLOAD_DMV_UNDER"))"
 			mv -f "$zip_file" "$UPLOAD_DMV_UNDER"
 		else
 			TOTAL_OVER_FOLDER=$(($TOTAL_OVER_FOLDER + 1))
 
 			zip_file="$dir_name/$folder_name".zip
-			echo "Start zipping:$zip_file"
+			echo "Start zipping:$folder_name.zip"
 			zip -r "$zip_file" "$dir" >/dev/null 2>&1
-			echo "Moving zip file to: $UPLOAD_DMV_OVER"
+			size=$(get_file_size "$zip_file")
+			echo "Moving zip file ($(convert_size "$size")) to: $(echo $(basename "$UPLOAD_DMV_OVER"))"
 			mv -f "$zip_file" "$UPLOAD_DMV_OVER"
 		fi
 		echo "Moving original folder to backup folder"
